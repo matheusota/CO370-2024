fb39422f344e91d335295f798588241e14337764
matheusota <matheusota@gmail.com>
2024-09-11 15:17:06 -0400

    Added Tutorial 2

--- code/gendata.py
+++ code/gendata.py
@@ -1,30 +1,27 @@
-
-
 import gurobipy as gp
 from gurobipy import GRB
-
 
 
 m = gp.Model("gendata")
 
 
-f = open("data2.txt", "rt")
+f = open("../data/data2.txt", "r")
 
 data = f.readlines()
 
-n = len( data )
+n = len(data)
 
 objs = []
 coeffs = []
 
 for line in data:
-    [ varobj, varcoeff ] = line.split(",")
-    objs.append( float( varobj ) )
-    coeffs.append( float( varcoeff ) )
+    [varobj, varcoeff] = line.split(",")
+    objs.append(float(varobj))
+    coeffs.append(float(varcoeff))
 
-print( data )
-print( objs )
-print( coeffs )
+print(data)
+print(objs)
+print(coeffs)
 
 # Model for the following LP:
 # maximize sum_{i=1}^n objs[i] * x[i]
@@ -36,26 +33,21 @@ print( coeffs )
 # and are of type GRB.CONTINUOUS. You may change these parameters (they are optional)
 
 # Here we are creating n variables that are continuous, with name x...
-x = m.addVars( n, vtype = GRB.CONTINUOUS, name = "x" )
+x = m.addVars(n, vtype=GRB.CONTINUOUS, name="x")
 
-m.setObjective( x.prod( objs ), GRB.MAXIMIZE )
+m.setObjective(x.prod(objs), GRB.MAXIMIZE)
 
 rhs = 10.0
 
-c1 = m.addConstr( x.prod( coeffs ) <= rhs, "c1")
+c1 = m.addConstr(x.prod(coeffs) <= rhs, "c1")
 
 m.optimize()
 
 if m.status == GRB.OPTIMAL:
-   print("\n**** SOLUTION OBTAINED FROM GUROBI ****")
-   print("Cost: %g" % m.ObjVal)
-   print("Vars:")
-   for i in range( n ):
-      print(x[i].VarName +  " : " + str( x[i].X ) )
+    print("\n**** SOLUTION OBTAINED FROM GUROBI ****")
+    print("Cost: %g" % m.ObjVal)
+    print("Vars:")
+    for i in range(n):
+        print(x[i].VarName + " : " + str(x[i].X))
 else:
-   print("No solution")
-
-
-
-
-
+    print("No solution")
